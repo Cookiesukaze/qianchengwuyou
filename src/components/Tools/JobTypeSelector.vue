19f0cd279a2f2616a6e3b8012cb73b633aad2982
Cookiesukaze <Cookiesukaze@qq.com>
2024-03-20 21:13:01 +0800

    重新调整了颜色with dist

--- src/components/Tools/JobTypeSelector.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-<!--    andmore-->
-  </div>
-</template>
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/reset.css'
-import '@/assets/font/font.css'
-const app = createApp(App)
-app.use(store).use(router).use(Antd).mount('#app')
